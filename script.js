6a1226ed93b4330b4eb1188472b35894828aaf53
Rajaddin Rachmadh <ftrajadr@gmail.com>
2026-05-22 04:49:09 +0700

    feat: make layout fully responsive

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
-document.querySelectorAll(".Navigation-Button").forEach((item) => {
+const DashboardButton = document.querySelectorAll(".Navigation-Button");
+
+DashboardButton.forEach((item) => {
     item.addEventListener("click", () => {
-        document.querySelector(".Navigation-Container").classList.toggle("Hide");
+        document.querySelector(".Navigation-Container").classList.toggle("Hide")
     })
-})
+});
